74bd21ef284b473d1ddf9c53641ddf8d87a9dd03
marcus <mliljenberg@gmail.com>
2024-02-18 10:34:16 +0100

    update

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,6 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+-- if true then return {} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -151,6 +151,7 @@ return {
         "query",
         "regex",
         "tsx",
+        "rust",
         "typescript",
         "vim",
         "yaml",
